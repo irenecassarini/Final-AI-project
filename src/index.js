5db3fc1376ee637641505d1a08292c4eaf57b4b6
irenecassarini <47857296+irenecassarini@users.noreply.github.com>
2025-08-09 17:15:55 +0200

    first

---
+++ src/index.js
@@ -0,0 +1,28 @@
+function displayPoem(response){
+new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 10,
+    cursor: "",
+  });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "334ff5a8425o317bf171aa4a1a6b3b0t";
+    let context = "You are a wise old spirit that hates capitalism and loves short poems";
+    let prompt = `Please create a short poem with an anticapitalist perspective about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`; 
+ 
+  let poemElement = document.querySelector("#poem");
+   poemElement.classList.remove("hidden");
+   poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
+
