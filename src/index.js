e053c359616839e96d96332e112bccb8ca847437
irenecassarini <47857296+irenecassarini@users.noreply.github.com>
2025-08-09 17:20:15 +0200

    change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function displayPoem(response){
+function displayAffirmation(response){
 new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -12,15 +12,15 @@ function generatePoem(event) {
 
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "334ff5a8425o317bf171aa4a1a6b3b0t";
-    let context = "You are a wise old spirit that hates capitalism and loves short poems";
-    let prompt = `Please create a short poem with an anticapitalist perspective about ${instructionsInput.value}`;
+    let context = "You are a wise old witch that hates capitalism and loves self-confidence.";
+    let prompt = `Please create a short affirmation with an anticapitalist perspective about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`; 
  
   let poemElement = document.querySelector("#poem");
    poemElement.classList.remove("hidden");
-   poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
+   poemElement.innerHTML = `<div class="generating"> 💚 Generating an affirmation about ${instructionsInput.value}</div>`;
 
-  axios.get(apiURL).then(displayPoem);
+  axios.get(apiURL).then(displayAffirmation);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
